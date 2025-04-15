6cee636207132d5a6710d5954097091a082e7af1
abdlmd <abdulmueedhabibraja@gmail.com>
2025-04-15 09:53:20 +0500

    initial commit

---
+++ src/main/java/com/example/semesterProjectBackend/SemesterProjectBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.semesterProjectBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SemesterProjectBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SemesterProjectBackendApplication.class, args);
+	}
+
+}
